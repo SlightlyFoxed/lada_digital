49d6110c43af904dafc800432f094baadad88d8d
Kozlov Ilya <kozlovis@krlb.ru>
2025-09-26 05:59:11 +0300

    add waiting

--- src/test/java/pages/LadaDigitalPage.java
+++ src/test/java/pages/LadaDigitalPage.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 import data.enums.LadaDigit;
 import io.qameta.allure.Step;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -26,7 +28,7 @@ public class LadaDigitalPage {
     @Step("Открыть главную страницу 'Lada Цифра'")
     public LadaDigitalPage openPageLadaDigit(){
         open("");
-        titleOfMainPage.shouldBe(visible).shouldHave(text(String.valueOf(LadaDigit.HELLO_MESSAGE)));
+        titleOfMainPage.shouldBe(visible, Duration.ofSeconds(5)).shouldHave(text(String.valueOf(LadaDigit.HELLO_MESSAGE)));
         return this;
     }
 
